e5df994ec4926cde2f5e82a876ace5d3e7e0a16a
Tanasoaia Ana-Maria <tanasoaiaanamaria@gmail.com>
2022-10-18 17:36:40 +0300

    setup proiect

---
+++ src/main/java/com/usv/siriusvoleiapp/SiriusVoleiAppApplication.java
@@ -0,0 +1,12 @@
+package com.usv.siriusvoleiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriusVoleiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriusVoleiAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/usv/siriusvoleiapp/SiriusVoleiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usv.siriusvoleiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiriusVoleiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
