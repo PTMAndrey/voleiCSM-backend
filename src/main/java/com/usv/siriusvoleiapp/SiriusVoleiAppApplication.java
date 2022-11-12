2381ef3bb68ffdf7a5e2c6daf025434152e0292d
Tanasoaia Ana-Maria <tanasoaiaanamaria@gmail.com>
2022-11-12 12:31:34 +0200

    Configuring CORS in SpringBoot application

--- src/main/java/com/usv/siriusvoleiapp/SiriusVoleiAppApplication.java
+++ src/main/java/com/usv/siriusvoleiapp/SiriusVoleiAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000")
 public class SiriusVoleiAppApplication {
 
 	public static void main(String[] args) {
