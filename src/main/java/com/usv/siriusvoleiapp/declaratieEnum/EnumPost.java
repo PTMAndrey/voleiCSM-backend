54ca0d07d0fcfe254d005fa6ba8824b8120ad92c
Tanasoaia Ana-Maria <tanasoaiaanamaria@gmail.com>
2022-12-22 01:58:57 +0200

    refactoring

--- src/main/java/com/usv/siriusvoleiapp/declaratieEnum/EnumPost.java
+++ src/main/java/com/usv/siriusvoleiapp/declaratieEnum/EnumPost.java
@@ -1,5 +1,5 @@
 package com.usv.siriusvoleiapp.declaratieEnum;
 
 public enum EnumPost {
-    PRINCIPAL, SECUNDAR, CENTRU, OPUS, RIDICATOR, LIBERO, EXTREMA
+    PRINCIPAL, SECUNDAR, CENTRU, OPUS, RIDICATOR, LIBERO, EXTREMA, ANTRENOR
 }
